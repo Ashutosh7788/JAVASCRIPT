ecba4e606276d231753a378c7b3a41b8a7bc368f
Ashutosh Behera <151955640+Ashutosh7788@users.noreply.github.com>
2024-11-26 11:19:05 +0530

    Add variables in js

---
+++ L-3_Variables in JS/script.js
@@ -0,0 +1,9 @@
+console.log(firstname)
+var firstName = 'Ashutosh'
+let lastName='Behera'
+let age = 15
+ let isHappy = false
+ firstName = 9090
+ var a 
+ const hoursInDay = 24 
+ let userIntro = 'Hi, my name is '+ firstName+'' + 'lastName'+ '.I am' + age + ''+ 'years old.'
