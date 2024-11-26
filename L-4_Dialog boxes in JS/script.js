bb099bfcc7f67116495a80699432c8f6254edfee
Ashutosh Behera <151955640+Ashutosh7788@users.noreply.github.com>
2024-11-26 19:44:20 +0530

    Add Dialog boxes program

---
+++ L-4_Dialog boxes in JS/script.js
@@ -0,0 +1,4 @@
+//  const alertResult = alret("Your message goes here.")
+//  const isConfirmed = confirm("Wouldyou like to continue")
+ userInput= prompt("Enter your  message")
+ console.log(userInput)
