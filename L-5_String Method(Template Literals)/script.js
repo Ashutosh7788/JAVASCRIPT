97b9ec7d5f015f9163816a643808d62534a80389
Ashutosh Behera <151955640+Ashutosh7788@users.noreply.github.com>
2024-11-28 12:34:35 +0530

    Add files via upload

---
+++ L-5_String Method(Template Literals)/script.js
@@ -0,0 +1,21 @@
+const message = 'Hello world'
+const faltuMessage = '     Hi, I am Ashutosh.    '
+const finalMessage = faltuMessage.trim()
+const finalMessageLowerCase = faltuMessage.trim().toLocalLowerCase()
+const replaceMessage = finalMessage.replace('Hi', 'Hello')
+
+
+const lastFourDigits = '3996'
+const maskedAccountNumber = lastFoundDigits.padStart(16,'*')
+
+
+const capitalMessage = message.toUpperCase()
+const bankBalance = 45855
+
+
+const concatenatedStirng = `Last four digits of my account number is`.concat('',lastFourDigits)
+const templateStirng =`My account number is ${lastFourDigits}.`
+const templateStirng2 =`I have ₹${bankBalance} in my account.`
+
+
+const addedString = 'I have ₹' + bankBalance + 'in my account.'
